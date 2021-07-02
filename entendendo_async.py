7a91a0e1bf741dd98d91b53bccb03a2e6405b3c1
Gregorio Honorato <greghono@gmail.com>
2021-07-01 21:06:15 -0300

    AGORA SIM, ENTENDENDO O CONTEXTO

--- entendendo_async.py
+++ entendendo_async.py
@@ -1,17 +1,54 @@
-from asyncio import create_task, gather, get_event_loop, ensure_future, run, sleep
+from asyncio import create_task, gather, get_event_loop, run, sleep
 from random import randint, choice
+from datetime import datetime
+from requests import get
+from time import time
 
 
-async def limpar(tarefa):
+async def gravar_arquivo_txt(msg: str, tarefa: str, e: bool = True):
+    t = randint(1, 10) if e else 0
+    await sleep(t)
+    print(f"INICIANDO TAREFA {tarefa} E ESPERANDO {t}")
+    with open("/tmp/arq.txt", "a") as f:
+        f.write(
+            datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+            + f" {tarefa:0>3}:{t:0>3} -- "
+            + msg
+            + "\n"
+        )
+
+
+async def txt(tam_len, qtde_tarefas=5):
+    for t in range(qtde_tarefas):
+        exec(
+            "t_{} = create_task(gravar_arquivo_txt(''.join([chr(randint(65, 90)) for _ in range({})]), {}))".format(
+                t, tam_len, t
+            )
+        )
+    for t in range(qtde_tarefas):
+        await eval(f"t_{t}")
+
+
+async def limpar(tarefa: str):
     print(f"FAZENDO LIMPEZA {tarefa}\n")
     await sleep(0.1)
 
 
-async def worker(tarefa):
+async def worker(tarefa: str):
     tempo = randint(1, 5)
     await sleep(tempo)
-    print(f"Iteracao {tarefa}, tempo decorrido: {tempo}")
+    print(f"TAREFA: {tarefa}, TEMPO PARA CONCLUIR: {tempo}")
     await limpar(tarefa)
+
+
+async def t_coperative():
+    print("TAREFAS COPERATIVAS, PERCEBA QUE A PRIMEIRA SEMPRE RODA PRIMEIRO!")
+    print("-" * 100)
+    t1 = create_task(worker(1))
+    t2 = create_task(worker(2))
+
+    await t1
+    await t2
 
 
 async def ex_serie(coroutine_tasks):
@@ -29,9 +66,26 @@ async def ex_paralelo(coroutine_tasks):
     await gather(*workers)
 
 
-def ex_eterno(loop):
-    print("Jesus")
-    loop.call_later(1, ex_eterno, loop)
+class CoorepadoraNET:
+    def __init__(self, qtde_tasks: int):
+        run(self.run(qtde_tasks))
+
+    async def run(self, qtde_tasks: int):
+        for t in range(qtde_tasks):
+            exec("t_{} = create_task(self.pegar_dados({}))".format(t, t))
+        for t in range(qtde_tasks):
+            await eval(f"t_{t}")
+
+    async def pegar_dados(self, task):
+        print(await self.get(task))
+
+    async def get(self, task):
+        URL = "https://www.planoalgartelecom.com.br"
+        print(f"PEGANDO DADOS {URL} TAREFA:{task}")
+        try:
+            return get(URL)
+        except Exception as e:
+            print(f"ERRO OCORRIDO NA TAREFA {task}: {e}")
 
 
 TOTAL = 5
@@ -49,10 +103,27 @@ tarefas = [
 
 f = lambda x: worker(choice(tarefas) + f" {x}")
 
+t_ini = time()
+
 
 def main():
-    print("-" * 100)
-    if TIPO_RODAR == "run_serie":
+    print("-" * 500)
+    if TIPO_RODAR == "run_cooperative":
+        run(t_coperative())
+
+    elif TIPO_RODAR == "run_cooperative_net":
+        CoorepadoraNET(5)
+
+    elif TIPO_RODAR == "run_cooperative_txt":
+        run(txt(5000, 500))
+        run(gravar_arquivo_txt(f"TEMPO CORRIDO: {time() - t_ini} ", "FIM", False))
+
+        t_in = time()
+        for t in range(5000):
+            run(txt(5000, t))
+        run(gravar_arquivo_txt(f"TEMPO CORRIDO SERIE: {time() - t_in} ", "FIM", False))
+
+    elif TIPO_RODAR == "run_serie":
         run(ex_serie([f(x) for x in range(TOTAL)]))
         run(ex_paralelo([f(x) for x in range(TOTAL)]))
 
@@ -66,11 +137,6 @@ def main():
         loop.run_until_complete(ex_paralelo([f(x) for x in range(TOTAL)]))
         loop.close()
 
-    elif TIPO_RODAR == "loop_infinito":
-        loop = get_event_loop()
-        loop.call_soon(ex_eterno, loop)
-        loop.run_forever()
 
-
-TIPO_RODAR = "loop_infinito"
+TIPO_RODAR = "run_cooperative_net"
 main()
--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-import asyncio
-
-
-def print_and_repeat(n, loop):
-    print(f"Hello World {n}")
-    loop.call_later(2, print_and_repeat, 1, loop)
-
-
-loop = asyncio.get_event_loop()
-loop.call_soon(print_and_repeat, 1, loop)
-loop.run_forever()
+from requests import get
